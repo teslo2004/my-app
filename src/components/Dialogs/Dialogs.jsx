cb73705f0ccf99aed8907a6f8a95bc5040b01270
Alexander Tesliuk <teslo2004@mail.ru>
2019-07-23 13:58:35 +0300

    dialogs_18

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Dialogs.module.css';
+
+
+const Dialogs = (props) =>{
+    return(
+        <div>
+            Dialogs1
+        </div>
+
+    )
+}
+
+export default Dialogs;
