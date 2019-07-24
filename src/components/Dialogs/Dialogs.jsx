ef230a85a0d7787f25207c5db0992054a39a5c4f
Alexander Tesliuk <teslo2004@mail.ru>
2019-07-24 17:34:04 +0300

    dialogs user_22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 
 const Dialogs = (props) =>{
     return(
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItem}>
+                <div classes={classes.dialog}>
+                    <NavLink to='/dialogs/1' activeClassName={classes.activeLink}>Alex</NavLink>
+                </div>
+                <div classes={classes.dialog}>
+                <NavLink to='/dialogs/2' activeClassName={classes.activeLink}>Elena</NavLink>
+                </div>
+                <div classes={classes.dialog}>
+                <NavLink to='/dialogs/3' activeClassName={classes.activeLink}>Dima</NavLink>
+                </div>
+                <div classes={classes.dialog}>
+                <NavLink to='/dialogs/4' activeClassName={classes.activeLink}>Andrew</NavLink>
+                </div>
+                <div classes={classes.dialog}>
+                <NavLink to='/dialogs/5' activeClassName={classes.activeLink}>Olga</NavLink>
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi!</div>
+                <div className={classes.message}>How are you?</div>
+                <div className={classes.message}>I am fine!</div>
+            </div>
         </div>
 
     )
