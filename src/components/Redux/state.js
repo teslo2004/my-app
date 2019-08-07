3f9dad6a104c57de59d4d18d717e6397304a67c7
teslo2004 <teslo2004@gmail.com>
2019-08-08 00:30:06 +0200

    store 37

--- src/components/Redux/state.js
+++ src/components/Redux/state.js
@@ -1,48 +1,92 @@
-let rerenderTree = () =>{
-        console.log('State was changed')
-}
-let state ={
-    messagesPage:{
-        dialogData:[{id:'1', name:'Alex'},
-            {id:'2', name:'Elena'},
-            {id:'3', name:'Dima'},
-            {id:'4', name:'Andrew'},
-            {id:'5', name:'Olga'}
-            ],
-    messageData:[{id:'0', message:'Hi!'},
-            {id:'1', message:'How are you?'},
-            {id:'2', message:'I am fine!'}
-             ]
-    },
-    profilePage:{
-    postMessage:[{id: '1', message:'Hi, it is my first message', likeCount:'14'},
-             {id: '2', message:'Hi, how are you', likeCount:'4'},
-             {id: '3', message:'I am fine!', likeCount:'7'},
-             {id: '4', message:'Do not worry!', likeCount:'11'}
-             ],
-    newPostText: 'it-com'
-            }
+let store = {
+        _state: {
+                messagesPage: {
+                        dialogData: [{
+                                        id: '1',
+                                        name: 'Alex'
+                                },
+                                {
+                                        id: '2',
+                                        name: 'Elena'
+                                },
+                                {
+                                        id: '3',
+                                        name: 'Dima'
+                                },
+                                {
+                                        id: '4',
+                                        name: 'Andrew'
+                                },
+                                {
+                                        id: '5',
+                                        name: 'Olga'
+                                }
+                        ],
+                        messageData: [{
+                                        id: '0',
+                                        message: 'Hi!'
+                                },
+                                {
+                                        id: '1',
+                                        message: 'How are you?'
+                                },
+                                {
+                                        id: '2',
+                                        message: 'I am fine!'
+                                }
+                        ]
+                },
+                profilePage: {
+                        postMessage: [{
+                                        id: '1',
+                                        message: 'Hi, it is my first message',
+                                        likeCount: '14'
+                                },
+                                {
+                                        id: '2',
+                                        message: 'Hi, how are you',
+                                        likeCount: '4'
+                                },
+                                {
+                                        id: '3',
+                                        message: 'I am fine!',
+                                        likeCount: '7'
+                                },
+                                {
+                                        id: '4',
+                                        message: 'Do not worry!',
+                                        likeCount: '11'
+                                }
+                        ],
+                        newPostText: 'it-com'
+                }
+        },
+        getStore(){
+                return this._state;
+        },
+        _callSubscriber() {
+                console.log('State was changed');
+        },
+        addPost(){
+                let newPost = {
+                        id: 5,
+                        message: this._state.profilePage.newPostText,
+                        likeCount: 0
+                };
+                this._state.profilePage.postMessage.push(newPost);
+                this._state.profilePage.newPostText = '';
+                this._callSubscriber(this._state);
+
+        },
+        updateNewPostText(newText){
+                this._state.profilePage.newPostText = newText;
+                this._callSubscriber(this._state);
+        },
+        subscribe(observer){
+                this._callSubscriber = observer;
         }
-window.state = state;
-
-export const addPost = () =>{
-        let newPost = {
-                id: 5,
-                message: state.profilePage.newPostText,
-                likeCount: 0
-        };
-        state.profilePage.postMessage.push(newPost);
-        state.profilePage.newPostText='';
-        rerenderTree(state);
-        
-};
-
-export const updateNewPostText = (newText) =>{
-        state.profilePage.newPostText = newText;
-        rerenderTree(state);
-};
-
-export const subscribe = (observer) =>{
-        rerenderTree = observer;
 }
-export default state;
+
+
+window.store = store;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state,  {subscribe, updateNewPostText, addPost } from './components/Redux/state';
+import store from './components/Redux/state';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -12,14 +12,14 @@ export let rerenderTree=(state)=>{
 ReactDOM.render(
     <BrowserRouter>
         <App state={state} 
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
         />
     </BrowserRouter>  ,
     document.getElementById('root')
 )};
-rerenderTree(state);
-subscribe(rerenderTree);
+rerenderTree(store._state);
+store.subscribe(rerenderTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
