c20c2756b2d5fc5e44b85281f0e815d73c5d2866
Alexander Tesliuk <teslo2004@mail.ru>
2019-09-05 17:42:04 +0300

    follow/unfollow 62

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Users.module.css';
 import userPhoto from '../../assets/image/user.png';
 import {NavLink} from 'react-router-dom';
+import * as axios from 'axios';
 
 let Users = (props) =>{
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -29,8 +30,38 @@ let Users = (props) =>{
                          <div>
                              {
                                  u.followed
-                                     ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                     : <button onClick={() => { props.follow(u.id) }}>Follow</button>
+                                     ? <button onClick={() => 
+
+                                        { 
+                                         axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                             withCredentials: true,
+                                             headers:{
+                                                "API-KEY": "f8beabf7-c57c-4019-9ce5-201c48120cfc"
+                                             }
+                                         })
+                                             .then(response => {
+                                                 if (response.data.resultCode == 0) {
+                                                     props.unfollow(u.id);
+                                                 }
+                                             });
+                                            
+                                     }}>Unfollow</button>
+                                     : <button onClick={() => 
+                                     {
+                                         axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {},{
+                                             withCredentials: true,
+                                             headers:{
+                                                "API-KEY": "f8beabf7-c57c-4019-9ce5-201c48120cfc"
+                                             }
+                                         })
+                                             .then(response => {
+                                                 if(response.data.resultCode == 0){
+                                                    props.follow(u.id);
+                                                 }
+                                             });
+                                            
+                                        
+                                        }}>Follow</button>
                              }
                          </div>
                      </span>
